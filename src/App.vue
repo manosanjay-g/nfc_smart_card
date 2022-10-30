54f499cfe334fe27b101304e6b3622653c844452
manosanjay-g <manosanjay.web@gmail.com>
2022-10-31 00:29:18 +0530

    fixed height issue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import { useRouter, useRoute } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+let currentRoute = computed(() => {
+  return route.name;
+});
+
+const routePage = (pageName) => {
+  router.push(pageName);
+};
+</script>
 
 <template>
-  <div id="app" class="text-white bg-gray-900">
+  <div id="main-app" class="text-white bg-gray-900">
     <nav
       class="flex flex-row justify-center py-3 border-b-2 border-gray-700 bg-gray-900 sticky top-0 z-50"
     >
       <h3 class="text-2xl">AppName</h3>
     </nav>
-    <RouterView />
+    <RouterView class="relative min-h-screen" />
     <footer
-      class="flex flex-row justify-between px-16 py-3 border-t-2 border-gray-700 bg-gray-900 sticky bottom-0 z-50"
+      v-if="currentRoute != 'login'"
+      class="flex flex-row justify-between px-16 py-4 border-t-2 border-gray-700 bg-gray-900 sticky bottom-0 z-50"
     >
       <font-awesome-icon
-        icon="fa-brands fa-twitter"
-        class="cursor-pointer text-2xl hover:text-blue-700"
-        @click="openLink('https://twitter.com/_manosanjay')"
+        icon="fa-solid fa-home"
+        class="cursor-pointer text-xl hover:text-blue-700"
+        @click="routePage('/')"
       />
       <font-awesome-icon
-        icon="fa-brands fa-twitter"
-        class="cursor-pointer text-2xl hover:text-blue-700"
-        @click="openLink('https://twitter.com/_manosanjay')"
+        icon="fa-solid fa-add"
+        class="cursor-pointer text-xl hover:text-blue-700"
+        @click="routePage('/add')"
       />
       <font-awesome-icon
-        icon="fa-brands fa-twitter"
-        class="cursor-pointer text-2xl hover:text-blue-700"
-        @click="openLink('https://twitter.com/_manosanjay')"
+        icon="fa-solid fa-gear"
+        class="cursor-pointer text-xl hover:text-blue-700"
+        @click="routePage('/settings')"
       />
     </footer>
   </div>
