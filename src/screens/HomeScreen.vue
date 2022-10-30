5c312c76e94444ce60d50cfce5049770017b9771
manosanjay-g <manosanjay.web@gmail.com>
2022-10-30 23:43:52 +0530

    populated with some data

--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,8 +1,76 @@
 <script setup></script>
 
 <template>
-  <div class="justify-center flex items-center h-screen">
-    <div class="text-4xl">Home 👋🏼</div>
+  <div class="flex flex-col px-8 pb-8">
+    <section id="manageBranches" class="my-3">
+      <h3 class="text-2xl font-semibold">Branches</h3>
+      <ul class="flex flex-col">
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">1</p>
+          <p>Ashok Nagar</p>
+          <p>No.20,18th Avenue,Ashok Nagar,Chennai-83</p>
+        </li>
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">2</p>
+          <p>K.K Nagar</p>
+          <p>No.1,4th Street,K.K Nagar,Chennai-83</p>
+        </li>
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">3</p>
+          <p>Anna Nagar</p>
+          <p>No.4,16th Stree,Anna Nagar,Chennai-83</p>
+        </li>
+      </ul>
+      <p class="text-blue-600 text-center cursor-pointer hover:text-blue-800">
+        See more
+      </p>
+    </section>
+    <section id="manageBranchManagers" class="mb-3">
+      <h3 class="text-2xl font-semibold">Managers</h3>
+      <ul class="flex flex-col">
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">1</p>
+          <p>Ashok Nagar</p>
+          <p>No.20,18th Avenue,Ashok Nagar,Chennai-83</p>
+        </li>
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">2</p>
+          <p>K.K Nagar</p>
+          <p>No.1,4th Street,K.K Nagar,Chennai-83</p>
+        </li>
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">3</p>
+          <p>Anna Nagar</p>
+          <p>No.4,16th Stree,Anna Nagar,Chennai-83</p>
+        </li>
+      </ul>
+      <p class="text-blue-600 text-center cursor-pointer hover:text-blue-800">
+        See more
+      </p>
+    </section>
+    <section id="manageCustomers">
+      <h3 class="text-2xl font-semibold">Recent Customers</h3>
+      <ul class="flex flex-col">
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">1</p>
+          <p>Ashok Nagar</p>
+          <p>No.20,18th Avenue,Ashok Nagar,Chennai-83</p>
+        </li>
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">2</p>
+          <p>K.K Nagar</p>
+          <p>No.1,4th Street,K.K Nagar,Chennai-83</p>
+        </li>
+        <li class="bg-gray-700 py-3 px-5 my-3 rounded-lg flex flex-row gap-3">
+          <p class="font-medium">3</p>
+          <p>Anna Nagar</p>
+          <p>No.4,16th Stree,Anna Nagar,Chennai-83</p>
+        </li>
+      </ul>
+      <p class="text-blue-600 text-center cursor-pointer hover:text-blue-800">
+        See more
+      </p>
+    </section>
   </div>
 </template>
 
