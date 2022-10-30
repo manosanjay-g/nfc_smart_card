f3b46e9c8aeae2a98128e5bc431df399f8d7c9d0
manosanjay-g <manosanjay.web@gmail.com>
2022-10-30 23:43:38 +0530

    added top and bottom bars

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <script setup></script>
 
 <template>
-  <div id="app" class="text-white bg-gray-900 h-screen">
+  <div id="app" class="text-white bg-gray-900">
+    <nav
+      class="flex flex-row justify-center py-3 border-b-2 border-gray-700 bg-gray-900 sticky top-0 z-50"
+    >
+      <h3 class="text-2xl">AppName</h3>
+    </nav>
     <RouterView />
+    <footer
+      class="flex flex-row justify-between px-16 py-3 border-t-2 border-gray-700 bg-gray-900 sticky bottom-0 z-50"
+    >
+      <font-awesome-icon
+        icon="fa-brands fa-twitter"
+        class="cursor-pointer text-2xl hover:text-blue-700"
+        @click="openLink('https://twitter.com/_manosanjay')"
+      />
+      <font-awesome-icon
+        icon="fa-brands fa-twitter"
+        class="cursor-pointer text-2xl hover:text-blue-700"
+        @click="openLink('https://twitter.com/_manosanjay')"
+      />
+      <font-awesome-icon
+        icon="fa-brands fa-twitter"
+        class="cursor-pointer text-2xl hover:text-blue-700"
+        @click="openLink('https://twitter.com/_manosanjay')"
+      />
+    </footer>
   </div>
 </template>
 
