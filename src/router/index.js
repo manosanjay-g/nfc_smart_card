a4bb8e82db5de5b47d67d176e6f3bd76b4e53f83
manosanjay-g <manosanjay.web@gmail.com>
2022-10-31 00:29:03 +0530

    routed new screens

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 //Routes
 import HomeScreen from '../screens/HomeScreen.vue'
 import LoginScreen from '../screens/LoginScreen.vue'
-
+import AddScreen from '../screens/AddScreen.vue'
+import SettingsScreen from '../screens/SettingsScreen.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -16,6 +17,16 @@ const router = createRouter({
             path: '/login',
             name: 'login',
             component: LoginScreen
+        },
+        {
+            path: '/add',
+            name: 'add',
+            component: AddScreen
+        },
+        {
+            path: '/settings',
+            name: 'settings',
+            component: SettingsScreen
         },
     ]
 })
