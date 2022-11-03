55f2edccd47cb9bb799d488e9b004591cb1a0516
manosanjay-g <manosanjay.web@gmail.com>
2022-11-03 23:36:37 +0530

    added logout functionality

--- src/screens/SettingsScreen.vue
+++ src/screens/SettingsScreen.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="settings-screen" class="flex justify-center items-center">
-    <p>Settings Screen</p>
+    <p class="bg-red-600" @click="authStore.logout">Log out</p>
   </div>
 </template>
+
+<script setup>
+import { useAuthStore } from "../store/authStore";
+const authStore = useAuthStore();
+</script>
